9c5c9f50fa991a1d607d4596a2ff4a43af6f3937
Altynbekoff <tauiekiel@mail.ru>
2019-08-15 22:44:29 +0500

    init commit 1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCIn_MmWq_YlzDu77Y98_J4Jt2AD6oy1wY",
+    authDomain: "angular-image-gallery-ba4ce.firebaseapp.com",
+    databaseURL: "https://angular-image-gallery-ba4ce.firebaseio.com",
+    projectId: "angular-image-gallery-ba4ce",
+    storageBucket: "angular-image-gallery-ba4ce.appspot.com",
+    messagingSenderId: "824017356573",
+    appId: "1:824017356573:web:670afcda6cb4a773"
+  }
 };
 
 /*
